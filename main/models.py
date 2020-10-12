ea6f184e44d29981f514fc2d97f74b7c010cd9d7
lunaisnotaboy <her@mint.lgbt>
2020-10-12 16:56:33 +0000

    add stuff

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
+from django.db.models import Q, QuerySet, Max, F, Count, Case, When, Exists, OuterRef
+from django.utils import timezone
+from django.forms.models import model_to_dict
+from django.utils.dateformat import format
+from django.core.validators import RegexValidator, URLValidator
+from django.core.exceptions import ValidationError
+from datetime import timedelta, datetime, date, time
 
 # Create your models here.
